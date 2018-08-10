0d5ef9ecd02e93c33e3bd6753cd15d25f4c87245
David <9396157+davidguerreiro@users.noreply.github.com>
2018-08-10 15:10:34 +0100

    initial commit

---
+++ src/form/Form.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import FormRow from './FormRow';
+
+class Form extends Component {
+    render() {
+        return(
+            <div>
+                <h2>Registro</h2>
+                <FormRow/>
+            </div>
+        );
+    }
+}
+
+export default Form;
---
+++ src/form/FormRow.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class FormRow extends Component {
+    render() {
+        return(
+            <div>
+                <label>Label</label>
+                <input/>
+            </div>
+        );
+    }
+}
+
+export default FormRow;
